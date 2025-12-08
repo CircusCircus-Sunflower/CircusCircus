e19330d7504e5c9b40129bb4ce3a4c8c490c2e74
Asaw89 <alan691saw@aol.com>
2025-12-08 17:31:30 -0500

    added post_rt

---
+++ post_routes.py
@@ -0,0 +1,70 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import current_user
+from flask_login.utils import login_required
+import datetime
+from forum.models import Post, Comment, Subforum, valid_content, valid_title, db, generateLinkPath, error
+
+post_rt = Blueprint('post_routes', __name__, template_folder='templates')
+
+@post_rt.route('/subforum')
+def subforum():
+	subforum_id = int(request.args.get("sub"))
+	subforum = Subforum.query.filter(Subforum.id == subforum_id).first()
+	if not subforum:
+		return error("That subforum does not exist!")
+	posts = Post.query.filter(Post.subforum_id == subforum_id).order_by(Post.id.desc()).limit(50)
+	if not subforum.path:
+		subforumpath = generateLinkPath(subforum.id)
+
+	subforums = Subforum.query.filter(Subforum.parent_id == subforum_id).all()
+	return render_template("subforum.html", subforum=subforum, posts=posts, subforums=subforums, path=subforumpath)
+
+@post_rt.route('/addpost')
+@login_required
+def addpost():
+	subforum_id = int(request.args.get("sub"))
+	subforum = Subforum.query.filter(Subforum.id == subforum_id).first()
+	if not subforum:
+		return error("That subforum does not exist!")
+
+	return render_template("createpost.html", subforum=subforum)
+
+@post_rt.route('/viewpost')
+def viewpost():
+	postid = int(request.args.get("post"))
+	post = Post.query.filter(Post.id == postid).first()
+	if not post:
+		return error("That post does not exist!")
+	if not post.subforum.path:
+		subforumpath = generateLinkPath(post.subforum.id)
+	comments = Comment.query.filter(Comment.post_id == postid).order_by(Comment.id.desc()) # no need for scalability now
+	return render_template("viewpost.html", post=post, path=subforumpath, comments=comments)
+
+@post_rt.route('/action_post', methods=['POST'])
+@login_required
+def action_post():
+	subforum_id = int(request.args.get("sub"))
+	subforum = Subforum.query.filter(Subforum.id == subforum_id).first()
+	if not subforum:
+		return redirect(url_for("subforums"))
+
+	user = current_user
+	title = request.form['title']
+	content = request.form['content']
+	#check for valid posting
+	errors = []
+	retry = False
+	if not valid_title(title):
+		errors.append("Title must be between 4 and 140 characters long!")
+		retry = True
+	if not valid_content(content):
+		errors.append("Post must be between 10 and 5000 characters long!")
+		retry = True
+	if retry:
+		return render_template("createpost.html",subforum=subforum,  errors=errors)
+	post = Post(title, content, datetime.datetime.now())
+	subforum.posts.append(post)
+	user.posts.append(post)
+	db.session.commit()
+	return redirect("/viewpost?post=" + str(post.id))
+
