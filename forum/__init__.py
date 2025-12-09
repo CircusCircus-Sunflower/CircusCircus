b059d60095fab508ac68dd9d7b64c179f5d85484
imiles <ieshia.miles@gmail.com>
2025-12-09 16:26:11 -0500

    Registered auth blueprint in __init.py__ and tested loginform page

--- forum/__init__.py
+++ forum/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from forum.routes import rt
+from forum.auth import auth #NEW LINE imports auth blueprint
 
 def create_app():
     """Construct the core application."""
@@ -10,6 +11,7 @@ def create_app():
     # subforum_routes
     # etc
     app.register_blueprint(rt)
+    app.register_blueprint(auth) #NEW LINE registers auth blueprint
     # Set globals
     from forum.models import db
     db.init_app(app)
